bef9c56bf4584450061bed77f2ac52cca165bed1
Naufal Dwi Alrizqi <dwialrizqi@gmail.com>
2022-12-05 23:35:21 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shamo/pages/cart_page.dart';
+import 'package:shamo/pages/checkout_page.dart';
+import 'package:shamo/pages/checkout_success_page.dart';
+import 'package:shamo/pages/detail_chat_page.dart';
+import 'package:shamo/pages/edit_profile_page.dart';
+import 'package:shamo/pages/home/main_page.dart';
+import 'package:shamo/pages/product_page.dart';
+import 'package:shamo/pages/sign_in_page.dart';
+import 'package:shamo/pages/sign_up_page.dart';
+import 'package:shamo/pages/splash_page.dart';
+import 'package:shamo/providers/auth_provider.dart';
+import 'package:shamo/providers/product_provider.dart';
+
+void main() => {
+      HttpOverrides.global = MyHttpOverrides(),
+      runApp(MyApp()),
+    };
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => AuthProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ProductProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => SplashPage(),
+          '/sign-in': (context) => SignInPage(),
+          '/sign-up': (context) => SignUpPage(),
+          '/home': (context) => MainPage(),
+          '/detail-chat': (context) => DetailChatPage(),
+          '/edit-profile': (context) => EditProfilePage(),
+          '/product': (context) => ProductPage(),
+          '/cart': (context) => CartPage(),
+          '/checkout': (context) => CheckoutPage(),
+          '/checkout-success': (context) => CheckoutSuccessPage(),
+        },
+      ),
+    );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+import 'package:shamo/models/user_model.dart';
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  Future<UserModel> register({
+    String baseUrl = ('http://10.0.2.2:8000/api'),
+    String? name,
+    String? username,
+    String? email,
+    String? password,
+  }) async {
+    var url = Uri.parse('$baseUrl/register');
+    var headers = {'Content-Type': 'application/json'};
+    var body = jsonEncode({
+      'name': name,
+      'username': username,
+      'email': email,
+      'password': password,
+    });
+    var response = await http.post(
+      url,
+      headers: headers,
+      body: body,
+    );
+
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body)['data'];
+      UserModel user = UserModel.fromJson(data['user']);
+      user.token = 'Bearer ' + data['access_token'];
+
+      return user;
+    } else {
+      throw Exception('Gagal Register');
+    }
+  }
+
+  Future<UserModel> login({
+    String baseUrl = ('http://10.0.2.2:8000/api'),
+    String? email,
+    String? password,
+  }) async {
+    var url = Uri.parse('$baseUrl/login');
+    var headers = {'Content-Type': 'application/json'};
+    var body = jsonEncode({
+      'email': email,
+      'password': password,
+    });
+    var response = await http.post(
+      url,
+      headers: headers,
+      body: body,
+    );
+
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body)['data'];
+      UserModel user = UserModel.fromJson(data['user']);
+      user.token = 'Bearer ' + data['access_token'];
+
+      return user;
+    } else {
+      throw Exception('Gagal Login');
+    }
+  }
+}
